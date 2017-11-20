e196c4d62150f01f792375a1f70b9cbe4281a6ae
Andrew Kroll <krolla@gmail.com>
2017-11-19 22:14:43 -0500

    Set up basic "SmartCab"

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class mainPanel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.networkStatus = new System.Windows.Forms.Button();
+            this.networkMessage = new System.Windows.Forms.TextBox();
+            this.ctiAcelaLink = new System.IO.Ports.SerialPort(this.components);
+            this.ctiPoller = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // networkStatus
+            // 
+            this.networkStatus.Location = new System.Drawing.Point(12, 12);
+            this.networkStatus.Name = "networkStatus";
+            this.networkStatus.Size = new System.Drawing.Size(75, 23);
+            this.networkStatus.TabIndex = 0;
+            this.networkStatus.Text = "Offline";
+            this.networkStatus.UseVisualStyleBackColor = true;
+            this.networkStatus.Click += new System.EventHandler(this.NetworkStatus_Click);
+            // 
+            // networkMessage
+            // 
+            this.networkMessage.Enabled = false;
+            this.networkMessage.Location = new System.Drawing.Point(12, 302);
+            this.networkMessage.Name = "networkMessage";
+            this.networkMessage.Size = new System.Drawing.Size(260, 20);
+            this.networkMessage.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.trackBar2);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(21, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 147);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(7, 94);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(227, 45);
+            this.trackBar2.SmallChange = 5;
+            this.trackBar2.TabIndex = 3;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(7, 43);
+            this.trackBar1.Maximum = 7;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(227, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(93, 19);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Reverse?";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Brake?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // mainPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 334);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.networkMessage);
+            this.Controls.Add(this.networkStatus);
+            this.Name = "mainPanel";
+            this.Text = "CTI Controller Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button networkStatus;
+        private System.Windows.Forms.TextBox networkMessage;
+        private System.IO.Ports.SerialPort ctiAcelaLink;
+        private System.Windows.Forms.Timer ctiPoller;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class mainPanel : Form
+    {
+        bool isNetworkOnline = false;
+
+        SmartCab cab1;
+
+        public mainPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.cab1 = new SmartCab(1);
+        }
+
+        private void NetworkStatus_Click(object sender, EventArgs e)
+        {
+            // change state of the network
+            isNetworkOnline = !isNetworkOnline;
+
+            // if the network is online, start the timer to poll the serial port
+            ctiPoller.Enabled = isNetworkOnline;
+
+            // update label to know the status of the network
+            if (isNetworkOnline)
+            {
+                networkStatus.Text = "Online";
+            }
+            else
+            {
+                networkStatus.Text = "Offline";
+            }
+        }
+
+        private void updateCabStatus(SmartCab cab)
+        {
+            byte[] command = cab.GenerateCommand();
+            StringBuilder hex = new StringBuilder(command.Length * 2);
+            foreach (byte b in command)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            networkMessage.Text = hex.ToString();
+            networkMessage.Text = command[3].ToString() + '-' + Convert.ToString(command[4], 2).PadLeft(8, '0');
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.cab1.ToggleBrake();
+            this.updateCabStatus(this.cab1);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.cab1.ToggleDirection();
+            this.updateCabStatus(this.cab1);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.cab1.SetInertia((byte) trackBar1.Value);
+            this.updateCabStatus(this.cab1);
+        }
+        
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            this.cab1.SetSpeed((byte) trackBar2.Value);
+            this.updateCabStatus(this.cab1);
+        }
+    }
+
+    interface IAcellaCommand
+    {
+        byte[] GenerateCommand();
+    }
+
+    class SmartCab : IAcellaCommand
+    {
+        enum Directions { Forward, Reverse };
+        enum Brakes { Off, On };
+        enum IdleVoltages { DoNotApply, Apply };
+
+        byte index;
+        byte speed;
+        byte direction;
+        byte brake;
+        byte inertia;
+        byte idleVoltage;
+
+        public SmartCab(byte index)
+        {
+            this.index = index;
+            this.speed = 0;
+            this.direction = (byte)Directions.Forward;
+            this.brake = (byte)Brakes.On;
+            this.inertia = 0;
+            this.idleVoltage = (byte)IdleVoltages.Apply;
+        }
+
+        public byte[] GenerateCommand()
+        {
+            /*
+            byte[] command = new byte[5];
+            command[0] = Convert.ToByte("0a", 16); // op code
+            command[1] = 0;
+            command[2] = this.index;
+            command[3] = this.speed;
+            byte attributes = (byte)(this.inertia + (this.brake * 8) + (this.direction * 16) + (this.idleVoltage * 32));
+            command[4] = attributes;
+            */
+
+            byte[] command =
+            {
+                Convert.ToByte("0a", 16), // op code
+                0,
+                this.index,
+                this.speed,
+                (byte)(this.inertia + (this.brake * 8) + (this.direction * 16) + (this.idleVoltage * 32))
+            };
+
+            return command;
+        }
+
+        public void ToggleBrake()
+        {
+            this.brake = (this.brake == (byte)Brakes.On) ? (byte)Brakes.Off : (byte)Brakes.On;
+        }
+
+        public void ToggleDirection()
+        {
+            this.direction = (this.direction == (byte)Directions.Forward) ? (byte)Directions.Reverse : (byte)Directions.Forward;
+        }
+
+        public void SetInertia(byte inertia)
+        {
+            this.inertia = inertia;
+        }
+
+        public void SetSpeed(byte speed)
+        {
+            if (speed > 100)
+            {
+                speed = 100;
+            }
+            this.speed = speed;
+        }
+    }
+}
